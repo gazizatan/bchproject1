8d00f664a5ebb8ff58ea99a4758d029e39723c40
gazizatan <153696424+gazizatan@users.noreply.github.com>
2024-12-21 22:13:26 +0500

    Start

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-class Block {
-    constructor(index, previousHash, wishlist, timestamp = Date.now()) {
-        this.index = index;
-        this.previousHash = previousHash;
-        this.timestamp = timestamp;
-        this.wishlist = wishlist;
-        this.nonce = 0;
-        this.hash = this.computeHash();
-    }
-
-    computeHash() {
-        const data = `${this.index}${this.previousHash}${this.timestamp}${JSON.stringify(this.wishlist)}${this.nonce}`;
-        return Block.hash(data);
+class MerkleTree {
+    constructor(items) {
+        this.leaves = items.map((item) => MerkleTree.hash(item));
+        this.root = this.buildTree(this.leaves);
     }
 
     static hash(data) {
@@ -19,6 +10,46 @@ class Block {
             const char = data.charCodeAt(i);
             hash = (hash << 5) - hash + char;
             hash |= 0;
+        }
+        return hash.toString(16);
+    }
+
+    buildTree(leaves) {
+        if (leaves.length === 1) return leaves[0];
+
+        const parentNodes = [];
+        for (let i = 0; i < leaves.length; i += 2) {
+            const left = leaves[i];
+            const right = leaves[i + 1] || leaves[i];
+            const combinedHash = MerkleTree.hash(left + right);
+            parentNodes.push(combinedHash);
+        }
+        return this.buildTree(parentNodes);
+    }
+}
+
+class Block {
+    constructor(index, previousHash, wishlist, timestamp = Date.now()) {
+        this.index = index;
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.wishlist = wishlist;
+        this.merkleRoot = new MerkleTree(wishlist.map(JSON.stringify)).root;
+        this.nonce = 0;
+        this.hash = this.computeHash();
+    }
+
+    computeHash() {
+        const data = `${this.index}${this.previousHash}${this.timestamp}${this.merkleRoot}${this.nonce}`;
+        return Block.hash(data);
+    }
+
+    static hash(data) {
+        let hash = 0;
+        for (let i = 0; i < data.length; i++) {
+            const char = data.charCodeAt(i);
+            hash = (hash << 5) - hash + char;
+            hash |= 0; // Convert to 32-bit integer
         }
         return hash.toString(16);
     }
@@ -39,7 +70,7 @@ class WishlistBlockchain {
     }
 
     createGenesisBlock() {
-        return new Block(0, "0", "Genesis Wishlist");
+        return new Block(0, "0", []);
     }
 
     getLastBlock() {
@@ -67,11 +98,22 @@ class WishlistBlockchain {
         alert("Wishlist block mined successfully!");
     }
 
-    getChain() {
-        return this.chain;
+    isValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.computeHash()) {
+                return false;
+            }
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
 if (typeof module !== "undefined") {
-    module.exports = WishlistBlockchain;
+    module.exports = { WishlistBlockchain, MerkleTree };
 }
